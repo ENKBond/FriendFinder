246d5e2c2d2b72b3fbb49da52811181a7ef7c309
ENK Bond <43937176+ENKBond@users.noreply.github.com>
2019-02-11 13:26:32 -0700

    final, needs bug review

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,29 @@ module.exports = function(app) {
         res.json(friendData);
     });
 
+    //post to api
     app.post("/api/friends", function(req, res) {
+        let friend = req.body;
+        let match = {
+            name: "",
+            photo: "",
+            scoreDiff: 0
+        };
+        let difference =0;
+        for (let i = 0; i<friends.length; i++) {
+            let currentFriend = friends[i];
+            for (let j=0; j<currentFriend.scores.length;j++) {
+                difference += Math.abs(currentFriend.scores[j] - parseInt(friend.scores[j]));
+            }
 
+            if (match.scoreDiff < difference){
+                match.name = currentFriend.name;
+                match.photo = currentFriend.photo;
+                match.scoreDiff = difference;
+            }
+        }
+
+        friends.push(match);
+        res.json(match);
     });
-}
+};
